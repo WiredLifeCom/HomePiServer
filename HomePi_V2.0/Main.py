b60e5cb54707eb94d76725b26b8f0ace826ccbbc
James Camps <jamescamps@me.com>
2015-05-29 14:57:24 +0200

    Project version, however needs a lot of work to become less spagetti and more stable

--- HomePi_V2.0/Main.py
+++ HomePi_V2.0/Main.py
@@ -12,6 +12,29 @@ import sys
 
 
 app = Flask(__name__)
+variable = "10.1.10.169"
+
+class C(object):
+    def __init__(self):
+        self._x = None
+
+    @property
+    def x(self):
+        """I'm the 'x' property."""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    def get(self):
+        return self.x
+
+    def set(self, x):
+        self.x = x
+
+
+C = C()
 
 
 class JsonEncoder:
@@ -22,6 +45,7 @@ class JsonEncoder:
         result = json.dumps(self.userData.__dict__, sort_keys=True, indent=4)
         return result
 
+
 class Zone(object):
     arrival = ""
     departure = ""
@@ -29,9 +53,12 @@ class Zone(object):
     material = ""
     longitude = 0.0
     latitude = 0.0
+    numberOfMaterialBlocks = 0
+
 
 class User(object):
     username = ""
+
 
 class Unload(object):
     unload = ""
@@ -39,10 +66,12 @@ class Unload(object):
     user = User()
     zones = [Zone()]
 
+
 def as_Unload(d):
     i = Unload()
     i.__dict__.update(d)
     return i
+
 
 class imHomeNow(object):
     username = ""
@@ -56,12 +85,11 @@ def as_isHomeNow(d):
     return i
 
 
-
 def threadedFunction():
     mainRunning = True
     while mainRunning:
         loopPeople()
-        sleep(10)
+        sleep(4)
 
 
 # loop through json files and make a list, the call the are you there method to check if they are still home
@@ -78,33 +106,37 @@ def AreYouThere(name):
     jsonObject = json.loads(open(name).read(), object_hook=as_isHomeNow)
     address = jsonObject.ipAddress
     print "The address to ping: { ", address + " }"
-    if jsonObject.isHome == "true":
-        res = os.system("ping -n 1 " + address)
-        if res == 0:
-            print "***Ping to { ", address, " } had a response, user is home***"
-        else:
-            print "***Ping to { ", address, " } failed!***"
-            fileManager = open('{0}.json'.format(jsonObject.username), "w")
-            jsonObject.isHome = "false"
-            fileManager.writelines(json.dumps(jsonObject, default=lambda y: y.__dict__, indent=4, sort_keys=True))
-            fileManager.close()
-            r = requests.post("http://",mainPiIP,"/onlinestatus", data="json.dumps(jsonObject)")
-            return Response(status=r.status_code)
-            # Update MainPi that the user is ot home anymore
+    print variable
+    res = os.system("ping -n 3 " + address)
+    if res == 0:
+        senddata = ('{"username": "' + str(jsonObject.username) + '", "isHome": "true", "ipAddress": "' + str(
+            jsonObject.ipAddress) + '"}')
+        r = requests.post("http://"+variable+":7070/onlinestatus", data=senddata)
+        print "***Ping to { ", address, " } had a response, user is home***"
     else:
-        print "***The user with address : { ", address, " } is not home!***"
+        print "***Ping to { ", address, " } failed!***"
+        fileManager = open('{0}.json'.format(jsonObject.username), "w")
+        jsonObject.isHome = "false"
+        fileManager.writelines(json.dumps(jsonObject, default=lambda y: y.__dict__, indent=4, sort_keys=True))
+        fileManager.close()
+        senddata = ('{"username": "' + str(jsonObject.username) + '", "isHome": "false", "ipAddress": "' + str(
+            jsonObject.ipAddress) + '"}')
+        print senddata
+        r = requests.post("http://"+variable+":7070/onlinestatus", data=senddata)
+        return Response(status=r.status_code)
+        # Update MainPi that the user is ot home anymore
 
 
-#Method for app to say I'm home
+# Method for app to say I'm home
 #@app.route('/HoneyImHome', methods=['POST'])
 def SaveUserState(updateuser):
-        print "Save user state got this ***", updateuser
-        jsonObject = json.loads(updateuser, object_hook=as_isHomeNow)
-        fileManager= open('{0}.json'.format(jsonObject.username), "w")
-        fileManager.writelines(json.dumps(json.loads(updateuser), indent=4, sort_keys=True))
-        fileManager.close()
-        r = requests.post("http://",mainPiIP,"/onlinestatus", data=updateuser)
-        return Response(status=r.status_code)
+    print "Save user state got this ***", updateuser
+    jsonObject = json.loads(updateuser, object_hook=as_isHomeNow)
+    fileManager = open('{0}.json'.format(jsonObject.username), "w")
+    fileManager.writelines(json.dumps(json.loads(updateuser), indent=4, sort_keys=True))
+    fileManager.close()
+    r = requests.post("http://"+variable+":7070/onlinestatus", data=updateuser)
+    return Response(status=r.status_code)
 
 
 #Sends JSON package from mobile app to Main Pi Server
@@ -115,14 +147,12 @@ def SendPackageToMainPi():
         print "no good json request!!"
         return Response(status=422)
     print "unload *** ", request.data
-    r = requests.post("http://",mainPiIP,"/unload", data=request.data)
+    r = requests.post("http://"+variable+":7070/unload", data=request.data)
     print(r.status_code, r.reason)
     print "Unload *** This is ip", request.remote_addr
     SaveUserState(json.dumps({"username": jsonObject.user.username,
                               "isHome": "true", "ipAddress": request.remote_addr}))
     return Response(status=r.status_code)
-
-
 
 
 def exit_handler():
@@ -134,9 +164,11 @@ atexit.register(exit_handler)
 if __name__ == "__main__":
     app.debug = True
     thread = Thread(target=threadedFunction)
-    #thread.start()
+    thread.start()
     print 'argument list;', str(sys.argv)
     myIP = str(sys.argv[1])
-    mainPiIP = str(sys.argv[2])
+    C.set(str(sys.argv[2]))
+    print C.get()
+
     app.run(host=myIP, port=5000, debug=True, use_reloader=False)
 
--- HomePi_V2.0/PostRequest.py
+++ HomePi_V2.0/PostRequest.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     while True:
         requests.post("http://10.1.17.115:7070/unload", data='{"user": { "username": "bejbejpomp"}, "zones": [{ "arrival": "2015-04-21T11:42:11.000+02:00","departure": "2015-04-21T11:58:32.000+02:00", "latitude": 55.61592, "longitude": 12.987113, "material": "Dirt", "radius": 20}], "materials":["Dirt", "Dirt"],"unload": "2015-04-21T13:04:54.000+02:00"}')
 
-    #requests.post("http://10.1.17.115:7070/onlinestatus", data='{"username": "bejbejpomp", "isHome": false, "ipAddress": "1.1.1.1"}')
+    requests.post("http://10.1.17.115:7070/onlinestatus", data='{"username": "bejbejpomp", "isHome": false, "ipAddress": "1.1.1.1"}')
